6384ef0ae855061286a8dec80b5932ad615c777f
Charles Koduru <charleskoduru@gmail.com>
2026-09-24 14:04:14 +0000

    updated config.py removed .v4l2

--- src/telerobot/config.py
+++ src/telerobot/config.py
@@ -7,10 +7,7 @@ from pathlib import Path
 from typing import Any
 
 import yaml
-from lerobot.cameras.opencv.configuration_opencv import (
-    OpenCVCameraConfig,
-    Cv2Backends,
-)
+from lerobot.cameras.opencv.configuration_opencv import OpenCVCameraConfig
 
 from lerobot.robots.robot import Robot
 from lerobot.robots.so_follower import SOFollower
@@ -185,7 +182,6 @@ def load_robot(path: str | Path) -> tuple[Robot, RobotConfig]:
             width=cam.width,
             height=cam.height,
             fps=cam.fps,
-            backend=Cv2Backends.V4L2,
         )
         for name, cam in cfg.cameras.items()
     }
